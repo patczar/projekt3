37bdf6c753960dde6552361e89cd2fcc0a200284
Andrzej <2452@sdf>
2022-03-04 13:34:00 +0100

    Commit z IntelliJ v6

--- src/main/java/projekt3/andrzej1/Main.java
+++ src/main/java/projekt3/andrzej1/Main.java
@@ -3,5 +3,8 @@ package projekt3.andrzej1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test nowej klasy");
+        Oblicz x = new Oblicz(1,2);
+        x.setA(1);
+        x.setB(2);
     }
 }
