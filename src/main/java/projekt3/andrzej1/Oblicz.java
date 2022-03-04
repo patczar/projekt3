bf0e24e164ae60f7b7266eb8b2e5ab242db2222b
Andrzej <2452@sdf>
2022-03-04 13:11:33 +0100

    Commit z IntelliJ v3

---
+++ src/main/java/projekt3/andrzej1/Oblicz.java
@@ -0,0 +1,9 @@
+package projekt3.andrzej1;
+
+public class Oblicz {
+    public static void main(String[] args) {
+        System.out.println("Kolejna klasa");
+        System.out.println("Kolejna klasa 2");
+        System.out.println("Kolejna klasa 3");
+    }
+}
