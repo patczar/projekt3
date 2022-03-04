2a1a65d497380a9d9e074c795f02846f0a5d5cc3
Patryk Czarnik <patryk@patrykczarnik.net>
2022-03-04 13:10:43 +0100

    13:10

--- src/main/java/projekt3/Patryk.java
+++ src/main/java/projekt3/Patryk.java
@@ -3,5 +3,6 @@ package projekt3;
 public class Patryk {
     public static void main(String[] args) {
         System.out.println("Halo halo, to plik Patryka");
+        System.out.println("Nowa linia");
     }
 }
---
+++ src/main/java/projekt3/patryk1/InnaKlasa.java
@@ -0,0 +1,7 @@
+package projekt3.patryk1;
+
+public class InnaKlasa {
+    public static void main(String[] args) {
+        System.out.println("13:10");
+    }
+}
